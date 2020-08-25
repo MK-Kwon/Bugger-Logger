baee63cee8a6fd71d77c6df065cb120f03bd6e5c
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-08-25 12:41:29 +0930

    Added JAWSDB_URL for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
+const connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
     port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "burgers_db"
-});
+  });
+};
 
 connection.connect(function(err){
     if(err) {
