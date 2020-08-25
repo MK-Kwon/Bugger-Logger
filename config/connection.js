6a110a91edcaed53d348f378d22cb500a55d0427
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-08-25 19:11:29 +0930

    modified connection

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,9 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    host: "localhost",
+    user: "root",
+    password: "",
     database: "burgers_db"
   });
 };
